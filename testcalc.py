def9653ea48289f9117e5f8847c0174987f68cd1
Adithya <adithya@github.com>
2020-04-10 14:23:25 -0700

    random stuff

--- testcalc.py
+++ testcalc.py
@@ -1,11 +1,10 @@
 import unittest
-from calc import *
+from calc import add,mult
 
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(add(4,3),7)
-        self.assertTrue(True)
 
     def test_mult(self):
         self.assertEqual(mult(3,4),12)
