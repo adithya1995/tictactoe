053a61cf5c42bb5c2740bc298de5bfeebf71eb01
Adithya Karthikeyan <32382556+adithya1995@users.noreply.github.com>
2020-07-31 01:20:22 -0700

    Create testcal.py

---
+++ testcal.py
@@ -0,0 +1,16 @@
+import unittest
+from calc import add,mult
+
+class TestCalc(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(4,3),7)
+
+    def test_mult(self):
+        self.assertEqual(mult(3,4),12)
+
+    def test_random(self):
+        self.assertFalse(False)
+
+if __name__ == '__main__':
+    unittest.main()
